93a7db45c0429c2462eacc54c14db572617d1676
Erick Takeshi <erick.rezende@iugu.com>
2023-05-16 14:30:49 -0300

    fix lint and remove useless test from base module

--- spec/openpix/ruby_sdk_spec.rb
+++ spec/openpix/ruby_sdk_spec.rb
@@ -1,11 +1,7 @@
 # frozen_string_literal: true
 
 RSpec.describe Openpix::RubySdk do
-  it "has a version number" do
+  it 'has a version number' do
     expect(Openpix::RubySdk::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
   end
 end
