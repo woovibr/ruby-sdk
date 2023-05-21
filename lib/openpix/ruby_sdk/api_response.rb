dc5c36870632a00910b3565acc6d83afcf577a9a
Erick Takeshi <erick.rezende@iugu.com>
2023-05-21 20:02:16 -0300

    handle error

--- lib/openpix/ruby_sdk/api_response.rb
+++ lib/openpix/ruby_sdk/api_response.rb
@@ -4,12 +4,13 @@ module Openpix
   module RubySdk
     # An Object representing the response from a call to Woovi API
     class ApiResponse
-      attr_reader :success, :resource_response, :pagination_meta
+      attr_reader :success, :resource_response, :pagination_meta, :error_response
 
       def initialize(status:, resource_response: nil, error_response: nil, pagination_meta: {})
         @success = status == 200
         @resource_response = resource_response
         @pagination_meta = pagination_meta
+        @error_response = error_response
       end
     end
   end
