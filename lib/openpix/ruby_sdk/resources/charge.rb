1d6afa0690724a4ce34d5ee87c3fb6eaa6af60d0
Erick Takeshi <erick.rezende@iugu.com>
2023-05-22 00:21:14 -0300

    refact charge

--- lib/openpix/ruby_sdk/resources/charge.rb
+++ lib/openpix/ruby_sdk/resources/charge.rb
@@ -27,7 +27,7 @@ module Openpix
 
         # @param params [Hash{String => String, Number, Hash{String, Number}, Array<Hash{String, String}>}] the attributes for creating a Charge
         # @param rest [Hash] more attributes to be merged at the body, use this only for unsupported fields
-        def init_body(params = {}, rest = {})
+        def init_body(params: {}, rest: {})
           super(base_attrs: ATTRS, params: params, rest: rest)
         end
 
