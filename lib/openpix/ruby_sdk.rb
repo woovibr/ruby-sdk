8de00e3ad22df48e6a0b2e77ec6116b546ce5bbc
Erick Takeshi <erick.rezende@iugu.com>
2023-05-22 17:54:07 -0300

    clear imports from main module

--- lib/openpix/ruby_sdk.rb
+++ lib/openpix/ruby_sdk.rb
@@ -1,10 +1,9 @@
 # frozen_string_literal: true
 
-require_relative 'ruby_sdk/version'
-require_relative 'ruby_sdk/resources'
+require 'openpix/ruby_sdk/version'
+require 'openpix/ruby_sdk/client'
 
 module Openpix
   module RubySdk
-    # Your code goes here...
   end
 end
