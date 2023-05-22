13dde23ffc073a5e746448ba5abcca95c4282edd
Erick Takeshi <erick.rezende@iugu.com>
2023-05-22 18:55:46 -0300

    added status response to the attrs readable

--- lib/openpix/ruby_sdk/api_response.rb
+++ lib/openpix/ruby_sdk/api_response.rb
@@ -4,10 +4,11 @@ module Openpix
   module RubySdk
     # An Object representing the response from a call to Woovi API
     class ApiResponse
-      attr_reader :success, :resource_response, :pagination_meta, :error_response
+      attr_reader :success, :resource_response, :pagination_meta, :error_response, :status
 
       def initialize(status:, resource_response: nil, error_response: nil, pagination_meta: {})
         @success = status == 200
+        @status = status
         @resource_response = resource_response
         @pagination_meta = pagination_meta
         @error_response = error_response
