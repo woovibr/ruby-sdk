39f2d2493b55714cedc22c3b3b5d195f64e2163e
Erick Takeshi <erick.rezende@iugu.com>
2023-05-22 09:03:38 -0300

    implement to body and destroy actions

--- lib/openpix/ruby_sdk/resources/customer.rb
+++ lib/openpix/ruby_sdk/resources/customer.rb
@@ -19,7 +19,7 @@ module Openpix
 
         # @param params [Hash{String => String, Number, Hash{String, Number}, Array<Hash{String, String}>}] the attributes for creating a Charge
         # @param rest [Hash] more attributes to be merged at the body, use this only for unsupported fields
-        def init_body(params = {}, rest = {})
+        def init_body(params: {}, rest: {})
           super(base_attrs: ATTRS, params: params, rest: rest)
         end
 
@@ -30,6 +30,37 @@ module Openpix
         def to_url
           'customer'
         end
+
+        def to_body
+          body = super
+
+          return body if body['address'].nil? || body['address'].empty?
+
+          address_parsed = {}
+          body['address']&.each do |attr, value|
+            address_parsed[attr.camelize(:lower).gsub('Id', 'ID')] = value
+          end
+
+          body['address'] = address_parsed
+
+          body
+        end
+
+        # rubocop:disable Lint/UnusedMethodArgument
+        def destroy(id:)
+          raise(
+            ActionNotImplementedError,
+            'customer does not implement DELETE action'
+          )
+        end
+
+        def destroy!(id:)
+          raise(
+            ActionNotImplementedError,
+            'customer does not implement DELETE action'
+          )
+        end
+        # rubocop:enable Lint/UnusedMethodArgument
       end
     end
   end
