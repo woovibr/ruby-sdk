fd9dcb990479126e4c53e85b13e54d384a919525
Erick Takeshi <erick.rezende@iugu.com>
2023-05-22 08:28:01 -0300

    fix to body representation from charge resource

--- lib/openpix/ruby_sdk/resources/charge.rb
+++ lib/openpix/ruby_sdk/resources/charge.rb
@@ -45,17 +45,22 @@ module Openpix
         def to_body
           body = super
 
+          return body if body['customer'].nil? || body['customer'].empty?
+
           customer_parsed = {}
-          body['customer']&.each do |attr, value|
+          body['customer'].each do |attr, value|
             customer_parsed[attr.camelize(:lower).gsub('Id', 'ID')] = value
           end
 
+          body['customer'] = customer_parsed
+
+          return body if body['customer']['address'].nil? || body['customer']['address'].empty?
+
           customer_address_parsed = {}
-          body.dig('customer', 'address')&.each do |attr, value|
+          body['customer']['address'].each do |attr, value|
             customer_address_parsed[attr.camelize(:lower).gsub('Id', 'ID')] = value
           end
 
-          body['customer'] = customer_parsed
           body['customer'] = body['customer'].merge({ 'address' => customer_address_parsed })
 
           body
